106ecd375de5a12b3ea81b11019a6231ea75befe
dongdongbh <18310682633@163.com>
2026-02-21 15:20:30 -0500

    fix(mobile): use string resources for app shortcuts

--- apps/mobile/plugins/android-app-shortcuts.js
+++ apps/mobile/plugins/android-app-shortcuts.js
@@ -5,6 +5,7 @@ const { withAndroidManifest, withDangerousMod } = require('@expo/config-plugins'
 const MAIN_ACTIVITY_SUFFIX = '.MainActivity';
 const SHORTCUTS_RESOURCE = '@xml/mindwtr_shortcuts';
 const SHORTCUTS_FILE_NAME = 'mindwtr_shortcuts.xml';
+const SHORTCUTS_STRINGS_FILE_NAME = 'mindwtr_shortcuts_strings.xml';
 
 const SHORTCUTS_XML = `<?xml version="1.0" encoding="utf-8"?>
 <shortcuts xmlns:android="http://schemas.android.com/apk/res/android">
@@ -12,8 +13,8 @@ const SHORTCUTS_XML = `<?xml version="1.0" encoding="utf-8"?>
     android:enabled="true"
     android:icon="@mipmap/ic_launcher"
     android:shortcutId="add_task_inbox"
-    android:shortcutLongLabel="Add task to Inbox"
-    android:shortcutShortLabel="Add task">
+    android:shortcutLongLabel="@string/shortcut_add_task_long"
+    android:shortcutShortLabel="@string/shortcut_add_task_short">
     <intent
       android:action="android.intent.action.VIEW"
       android:data="mindwtr:///capture-quick?mode=text" />
@@ -22,8 +23,8 @@ const SHORTCUTS_XML = `<?xml version="1.0" encoding="utf-8"?>
     android:enabled="true"
     android:icon="@mipmap/ic_launcher"
     android:shortcutId="open_focus"
-    android:shortcutLongLabel="Open Focus view"
-    android:shortcutShortLabel="Focus">
+    android:shortcutLongLabel="@string/shortcut_open_focus_long"
+    android:shortcutShortLabel="@string/shortcut_open_focus_short">
     <intent
       android:action="android.intent.action.VIEW"
       android:data="mindwtr:///focus" />
@@ -32,13 +33,24 @@ const SHORTCUTS_XML = `<?xml version="1.0" encoding="utf-8"?>
     android:enabled="true"
     android:icon="@mipmap/ic_launcher"
     android:shortcutId="open_calendar"
-    android:shortcutLongLabel="Open Calendar view"
-    android:shortcutShortLabel="Calendar">
+    android:shortcutLongLabel="@string/shortcut_open_calendar_long"
+    android:shortcutShortLabel="@string/shortcut_open_calendar_short">
     <intent
       android:action="android.intent.action.VIEW"
       android:data="mindwtr:///calendar" />
   </shortcut>
 </shortcuts>
+`;
+
+const SHORTCUTS_STRINGS_XML = `<?xml version="1.0" encoding="utf-8"?>
+<resources>
+  <string name="shortcut_add_task_long" translatable="false">Add task to Inbox</string>
+  <string name="shortcut_add_task_short" translatable="false">Add task</string>
+  <string name="shortcut_open_focus_long" translatable="false">Open Focus view</string>
+  <string name="shortcut_open_focus_short" translatable="false">Focus</string>
+  <string name="shortcut_open_calendar_long" translatable="false">Open Calendar view</string>
+  <string name="shortcut_open_calendar_short" translatable="false">Calendar</string>
+</resources>
 `;
 
 const isMainActivity = (activityName) =>
@@ -84,8 +96,11 @@ module.exports = function withAndroidAppShortcuts(config) {
     'android',
     async (cfg) => {
       const xmlDir = path.join(cfg.modRequest.platformProjectRoot, 'app', 'src', 'main', 'res', 'xml');
+      const valuesDir = path.join(cfg.modRequest.platformProjectRoot, 'app', 'src', 'main', 'res', 'values');
       await fs.promises.mkdir(xmlDir, { recursive: true });
+      await fs.promises.mkdir(valuesDir, { recursive: true });
       await fs.promises.writeFile(path.join(xmlDir, SHORTCUTS_FILE_NAME), SHORTCUTS_XML, 'utf8');
+      await fs.promises.writeFile(path.join(valuesDir, SHORTCUTS_STRINGS_FILE_NAME), SHORTCUTS_STRINGS_XML, 'utf8');
       return cfg;
     },
   ]);
